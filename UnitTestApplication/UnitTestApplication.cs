98ca122e4b9dc9b749b873ae26b344dc6b1002c3
Freddy Juhel <domaine75@gmail.com>
2017-11-22 21:48:10 +0100

    Initial commit

---
+++ UnitTestApplication/UnitTestApplication.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using testZipFiles;
+namespace UnitTestApplication
+{
+  [TestClass]
+  public class UnitTestApplication
+  {
+    [TestMethod]
+    public void TestMethodDeletable_file_name_is_ok()
+    {
+      PrivateType privateTypeObject = new PrivateType(typeof(Program));
+      const string methodName = "Deletable";
+      const string source = "logs_20171103";
+      const bool expected = true;
+      object obj = privateTypeObject.InvokeStatic(methodName, source);
+      Assert.AreEqual(expected, (bool)obj);
+    }
+
+    [TestMethod]
+    public void TestMethodDeletable_file_name_is_ok2()
+    {
+      PrivateType privateTypeObject = new PrivateType(typeof(Program));
+      const string methodName = "Deletable";
+      const string source = "messages.log20171103";
+      const bool expected = true;
+      object obj = privateTypeObject.InvokeStatic(methodName, source);
+      Assert.AreEqual(expected, (bool)obj);
+    }
+    
+    [TestMethod]
+    public void TestMethodDeletable_file_name_is_ko_txt()
+    {
+      PrivateType privateTypeObject = new PrivateType(typeof(Program));
+      const string methodName = "Deletable";
+      const string source = "messageslog20171103_test.txt";
+      const bool expected = false;
+      object obj = privateTypeObject.InvokeStatic(methodName, source);
+      Assert.AreEqual(expected, (bool)obj);
+    }
+
+    [TestMethod]
+    public void TestMethodDeletable_file_name_is_ko_zip()
+    {
+      PrivateType privateTypeObject = new PrivateType(typeof(Program));
+      const string methodName = "Deletable";
+      const string source = "messageslog20171103.zip";
+      const bool expected = false;
+      object obj = privateTypeObject.InvokeStatic(methodName, source);
+      Assert.AreEqual(expected, (bool)obj);
+    }
+  }
+}
---
+++ UnitTestApplication/UnitTestHIMZipLogFiles.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zipFunctions = testZipFiles.Program;
+namespace UnitTestApplication
+{
+    [TestClass]
+    public class UnitTestHIMZipLogFiles
+    {
+        #region FileIsDeletable
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_length_smaller_than_8_characters()
+        {
+            const string source = "log2017";
+            const bool expected = false;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_wrong_year()
+        {
+            const string source = "log_201711";
+            const bool expected = false;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_wrong_month()
+        {
+            const string source = "log_2017mm20";
+            const bool expected = false;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_wrong_day()
+        {
+            const string source = "log_201711dd";
+            const bool expected = false;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_wrong_date()
+        {
+            const string source = "log_20170231";
+            const bool expected = false;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_correct_date_but_not_deletable()
+        {
+            const string source = "log_99991231";
+            const bool expected = false;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        [TestMethod]
+        public void TestMethod_FileIsDeletable_correct_date_but_deletable()
+        {
+            DateTime yesterday = DateTime.Now.AddDays(-1000);
+            string month = yesterday.Month.ToString().Length == 1 ? "0" + yesterday.Month : yesterday.Month.ToString();
+            string day = yesterday.Day.ToString().Length == 1 ? "0" + yesterday.Day : yesterday.Day.ToString();
+            string source = $"log_{yesterday.Year}{month}{day}";
+            const bool expected = true;
+            bool result = zipFunctions.FileIsDeletable(source);
+            Assert.AreEqual(result, expected);
+        }
+
+        #endregion FileIsDeletable
+        #region GetListOfDatesFromfileNames
+
+        [TestMethod]
+        public void TestMethod_GetListOfDatesFromfileNames_correct()
+        {
+            string workingPath = $"{Path.GetTempPath()}\\HimUnitTests" ;
+            PrepareDirectoryTest(workingPath);
+            List<string> expected = new List<string> { "99991231", "20171121", "20160101", "20150102", "20141121" };
+            //clean up temp directory before running unit tests
+            foreach (string file in expected)
+            {
+                if (File.Exists(Path.Combine(workingPath, $"log_{file}")))
+                {
+                    File.Delete(Path.Combine(workingPath, $"log_{file}"));
+                }
+            }
+
+            List<string> source2 = new List<string> { "log_99991231", "log_20171121", "log_20160101", "log_20150102", "log_20141121" };
+            foreach (string file in source2)
+            {
+                if (!File.Exists(file))
+                {
+                    using (File.Create(Path.Combine(workingPath, file)))
+                    {
+                        //just create a new file
+                    }
+                }
+            }
+
+            List<string> result = zipFunctions.GetListOfDatesFromfileNames(workingPath);
+            expected.Sort();
+            result.Sort();
+            for (int i = 0; i < result.Count; i++)
+            {
+                Assert.AreEqual(expected[i], result[i]);
+            }
+
+            //clean-up
+            foreach (string file in source2)
+            {
+                if (File.Exists(Path.Combine(workingPath, file)))
+                {
+                    File.Delete(Path.Combine(workingPath, file));
+                }
+            }
+        }
+
+        private void PrepareDirectoryTest(string directoryPath)
+        {
+            //delete all files in HimUnitTest directory if it exists or
+            // create a brand new HimUnitTest directory 
+            if (Directory.Exists(directoryPath))
+            {
+                // delete all files
+                DirectoryInfo directory = new DirectoryInfo(directoryPath);
+                List<string> allFiles = directory.GetFiles("*").Select(f=>f.Name).ToList();
+                foreach (string file in allFiles)
+                {
+                    if (File.Exists(file))
+                    {
+                        File.Delete(file);
+                    }
+                }
+            }
+            else
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_GetListOfDatesFromfileNames_not_correct_because_result_count_is_different()
+        {
+            string workingPath = $"{Path.GetTempPath()}\\HimUnitTests";
+            PrepareDirectoryTest(workingPath);
+            List<string> expected = new List<string> { "99991231", "20171121", "20160101", "20150102", "20141121" };
+            foreach (string file in expected)
+            {
+                if (File.Exists(Path.Combine(workingPath, $"log_{file}")))
+                {
+                    File.Delete(Path.Combine(workingPath, $"log_{file}"));
+                }
+            }
+
+            List<string> source2 = new List<string> { "log_99991231", "log_20160101", "log_20150102", "log_20141121" };
+            foreach (string file in source2)
+            {
+                if (!File.Exists(Path.Combine(workingPath, file)))
+                {
+                    using (File.Create(Path.Combine(workingPath, file)))
+                    {
+                        //just create a new file
+                    }
+                }
+            }
+
+            List<string> result = zipFunctions.GetListOfDatesFromfileNames(workingPath);
+            expected.Sort();
+            result.Sort();
+            bool finalResult = true;
+            if (expected.Count == result.Count)
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    if (expected[i] != result[i])
+                    {
+                        finalResult = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                finalResult = false;
+            }
+
+            Assert.IsFalse(finalResult);
+
+            //clean-up
+            foreach (string file in source2)
+            {
+                if (File.Exists(file))
+                {
+                    File.Delete(Path.Combine(workingPath, file));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod_GetListOfDatesFromfileNames_not_correct_because_items_are_different()
+        {
+            string workingPath = $"{Path.GetTempPath()}\\HimUnitTests";
+            PrepareDirectoryTest(workingPath);
+            List<string> expected = new List<string> { "99991231", "20171121", "20160101", "20150102", "20141121" };
+            //Clean up temp directory before running unit tests
+            foreach (string file in expected)
+            {
+                if (File.Exists(Path.Combine(workingPath, $"log_{file}")))
+                {
+                    File.Delete(Path.Combine(workingPath, $"log_{file}"));
+                }
+            }
+
+            List<string> source2 = new List<string> { "log_99991231", "log_20160101", "log_20150102", "log_20141121" };
+            foreach (string file in source2)
+            {
+                if (!File.Exists(Path.Combine(workingPath, file)))
+                {
+                    using (File.Create(Path.Combine(workingPath, file)))
+                    {
+                        //just create a new file
+                    }
+                }
+            }
+
+            List<string> result = zipFunctions.GetListOfDatesFromfileNames(workingPath);
+            expected.Sort();
+            result.Sort();
+            bool finalResult = true;
+            if (expected.Count == result.Count)
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    if (expected[i] != result[i])
+                    {
+                        finalResult = false;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                finalResult = false;
+            }
+
+            Assert.IsFalse(finalResult);
+
+            //clean-up
+            foreach (string file in source2)
+            {
+                if (File.Exists(file))
+                {
+                    File.Delete(Path.Combine(workingPath, file));
+                }
+            }
+        }
+
+        #endregion GetListOfDatesFromfileNames
+        #region ZipLogFiles
+
+        #endregion ZipLogFiles
+    }
+}
